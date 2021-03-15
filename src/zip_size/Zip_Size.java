d7aed80fe119a9a6703241f6418bb4c6da92f6c7
IoannisAsimakopoulos <john@192.168.181.1>
2021-03-15 18:27:38 +0200

    new commit

---
+++ src/zip_size/Zip_Size.java
@@ -0,0 +1,11 @@
+
+package zip_size;
+
+public class Zip_Size {
+
+    
+    public static void main(String[] args) {
+        new ZipSize().setVisible(true); // enable the main form 
+    }
+    
+}
